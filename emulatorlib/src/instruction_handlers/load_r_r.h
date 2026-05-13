42720f5abb3a6c00e768c47c3d529391263533d5
Tim Herreijgers <timtim3001@gmail.com>
2026-05-14 00:15:53 +0200

    Added load reg to reg instructions

---
+++ emulatorlib/src/instruction_handlers/load_r_r.h
@@ -0,0 +1,80 @@
+/*
+ * Copyright © 2026. Tim Herreijgers
+ * Licensed using the MIT license
+ */
+
+#pragma once
+
+#include "emulatorlib/address_bus.h"
+#include "emulatorlib/cpu_registers.h"
+#include "emulatorlib/instruction_handler.h"
+
+#include "instruction_handlers/register_concepts.h"
+#include "instruction_handlers/register_io_helpers.h"
+
+namespace EmulatorLib
+{
+
+template <ReturnsRegister8Bit WriteLocation, ReturnsRegister8Bit SourceLocation>
+constexpr auto ExecuteLoadRegReg = [](const AddressBus& /*addressBus*/, CpuRegisters& cpuRegisters) -> InstructionHandler {
+    WriteLocation{}(cpuRegisters) = SourceLocation{}(cpuRegisters).value;
+    co_return;
+};
+
+constexpr auto ExecuteLoad_A_A = ExecuteLoadRegReg<RegisterA, RegisterA>;
+constexpr auto ExecuteLoad_A_B = ExecuteLoadRegReg<RegisterA, RegisterB>;
+constexpr auto ExecuteLoad_A_C = ExecuteLoadRegReg<RegisterA, RegisterC>;
+constexpr auto ExecuteLoad_A_D = ExecuteLoadRegReg<RegisterA, RegisterD>;
+constexpr auto ExecuteLoad_A_E = ExecuteLoadRegReg<RegisterA, RegisterE>;
+constexpr auto ExecuteLoad_A_H = ExecuteLoadRegReg<RegisterA, RegisterH>;
+constexpr auto ExecuteLoad_A_L = ExecuteLoadRegReg<RegisterA, RegisterL>;
+
+constexpr auto ExecuteLoad_B_A = ExecuteLoadRegReg<RegisterB, RegisterA>;
+constexpr auto ExecuteLoad_B_B = ExecuteLoadRegReg<RegisterB, RegisterB>;
+constexpr auto ExecuteLoad_B_C = ExecuteLoadRegReg<RegisterB, RegisterC>;
+constexpr auto ExecuteLoad_B_D = ExecuteLoadRegReg<RegisterB, RegisterD>;
+constexpr auto ExecuteLoad_B_E = ExecuteLoadRegReg<RegisterB, RegisterE>;
+constexpr auto ExecuteLoad_B_H = ExecuteLoadRegReg<RegisterB, RegisterH>;
+constexpr auto ExecuteLoad_B_L = ExecuteLoadRegReg<RegisterB, RegisterL>;
+
+constexpr auto ExecuteLoad_C_A = ExecuteLoadRegReg<RegisterC, RegisterA>;
+constexpr auto ExecuteLoad_C_B = ExecuteLoadRegReg<RegisterC, RegisterB>;
+constexpr auto ExecuteLoad_C_C = ExecuteLoadRegReg<RegisterC, RegisterC>;
+constexpr auto ExecuteLoad_C_D = ExecuteLoadRegReg<RegisterC, RegisterD>;
+constexpr auto ExecuteLoad_C_E = ExecuteLoadRegReg<RegisterC, RegisterE>;
+constexpr auto ExecuteLoad_C_H = ExecuteLoadRegReg<RegisterC, RegisterH>;
+constexpr auto ExecuteLoad_C_L = ExecuteLoadRegReg<RegisterC, RegisterL>;
+
+constexpr auto ExecuteLoad_D_A = ExecuteLoadRegReg<RegisterD, RegisterA>;
+constexpr auto ExecuteLoad_D_B = ExecuteLoadRegReg<RegisterD, RegisterB>;
+constexpr auto ExecuteLoad_D_C = ExecuteLoadRegReg<RegisterD, RegisterC>;
+constexpr auto ExecuteLoad_D_D = ExecuteLoadRegReg<RegisterD, RegisterD>;
+constexpr auto ExecuteLoad_D_E = ExecuteLoadRegReg<RegisterD, RegisterE>;
+constexpr auto ExecuteLoad_D_H = ExecuteLoadRegReg<RegisterD, RegisterH>;
+constexpr auto ExecuteLoad_D_L = ExecuteLoadRegReg<RegisterD, RegisterL>;
+
+constexpr auto ExecuteLoad_E_A = ExecuteLoadRegReg<RegisterE, RegisterA>;
+constexpr auto ExecuteLoad_E_B = ExecuteLoadRegReg<RegisterE, RegisterB>;
+constexpr auto ExecuteLoad_E_C = ExecuteLoadRegReg<RegisterE, RegisterC>;
+constexpr auto ExecuteLoad_E_D = ExecuteLoadRegReg<RegisterE, RegisterD>;
+constexpr auto ExecuteLoad_E_E = ExecuteLoadRegReg<RegisterE, RegisterE>;
+constexpr auto ExecuteLoad_E_H = ExecuteLoadRegReg<RegisterE, RegisterH>;
+constexpr auto ExecuteLoad_E_L = ExecuteLoadRegReg<RegisterE, RegisterL>;
+
+constexpr auto ExecuteLoad_H_A = ExecuteLoadRegReg<RegisterH, RegisterA>;
+constexpr auto ExecuteLoad_H_B = ExecuteLoadRegReg<RegisterH, RegisterB>;
+constexpr auto ExecuteLoad_H_C = ExecuteLoadRegReg<RegisterH, RegisterC>;
+constexpr auto ExecuteLoad_H_D = ExecuteLoadRegReg<RegisterH, RegisterD>;
+constexpr auto ExecuteLoad_H_E = ExecuteLoadRegReg<RegisterH, RegisterE>;
+constexpr auto ExecuteLoad_H_H = ExecuteLoadRegReg<RegisterH, RegisterH>;
+constexpr auto ExecuteLoad_H_L = ExecuteLoadRegReg<RegisterH, RegisterL>;
+
+constexpr auto ExecuteLoad_L_A = ExecuteLoadRegReg<RegisterL, RegisterA>;
+constexpr auto ExecuteLoad_L_B = ExecuteLoadRegReg<RegisterL, RegisterB>;
+constexpr auto ExecuteLoad_L_C = ExecuteLoadRegReg<RegisterL, RegisterC>;
+constexpr auto ExecuteLoad_L_D = ExecuteLoadRegReg<RegisterL, RegisterD>;
+constexpr auto ExecuteLoad_L_E = ExecuteLoadRegReg<RegisterL, RegisterE>;
+constexpr auto ExecuteLoad_L_H = ExecuteLoadRegReg<RegisterL, RegisterH>;
+constexpr auto ExecuteLoad_L_L = ExecuteLoadRegReg<RegisterL, RegisterL>;
+
+} // namespace EmulatorLib
--- emulatorlib/src/instruction_handlers/opcode_handler_map.h
+++ emulatorlib/src/instruction_handlers/opcode_handler_map.h
@@ -6,6 +6,7 @@
 #pragma once
 
 #include "emulatorlib/instruction_handler.h"
+#include "load_r_r.h"
 
 #include "instruction_handlers/load_a16_sp.h"
 #include "instruction_handlers/load_indirect_n8.h"
@@ -55,6 +56,52 @@ InstructionHandler ExecuteNoop(AddressBus& /*addressBus*/, CpuRegisters& /*cpuRe
     handlers[0x36] = ExecuteLoadIndirectHlN8;
     handlers[0x3E] = ExecuteLoadAn8;
 
+    handlers[0x40] = ExecuteLoad_B_B;
+    handlers[0x41] = ExecuteLoad_B_C;
+    handlers[0x42] = ExecuteLoad_B_D;
+    handlers[0x43] = ExecuteLoad_B_E;
+    handlers[0x44] = ExecuteLoad_B_H;
+    handlers[0x45] = ExecuteLoad_B_L;
+    handlers[0x47] = ExecuteLoad_B_A;
+    handlers[0x48] = ExecuteLoad_C_B;
+    handlers[0x49] = ExecuteLoad_C_C;
+    handlers[0x4A] = ExecuteLoad_C_D;
+    handlers[0x4B] = ExecuteLoad_C_E;
+    handlers[0x4C] = ExecuteLoad_C_H;
+    handlers[0x4D] = ExecuteLoad_C_L;
+    handlers[0x4F] = ExecuteLoad_C_A;
+
+
+    handlers[0x50] = ExecuteLoad_D_B;
+    handlers[0x51] = ExecuteLoad_D_C;
+    handlers[0x52] = ExecuteLoad_D_D;
+    handlers[0x53] = ExecuteLoad_D_E;
+    handlers[0x54] = ExecuteLoad_D_H;
+    handlers[0x55] = ExecuteLoad_D_L;
+    handlers[0x57] = ExecuteLoad_D_A;
+    handlers[0x58] = ExecuteLoad_E_B;
+    handlers[0x59] = ExecuteLoad_E_C;
+    handlers[0x5A] = ExecuteLoad_E_D;
+    handlers[0x5B] = ExecuteLoad_E_E;
+    handlers[0x5C] = ExecuteLoad_E_H;
+    handlers[0x5D] = ExecuteLoad_E_L;
+    handlers[0x5F] = ExecuteLoad_E_A;
+
+    handlers[0x60] = ExecuteLoad_H_B;
+    handlers[0x61] = ExecuteLoad_H_C;
+    handlers[0x62] = ExecuteLoad_H_D;
+    handlers[0x63] = ExecuteLoad_H_E;
+    handlers[0x64] = ExecuteLoad_H_H;
+    handlers[0x65] = ExecuteLoad_H_L;
+    handlers[0x67] = ExecuteLoad_H_A;
+    handlers[0x68] = ExecuteLoad_L_B;
+    handlers[0x69] = ExecuteLoad_L_C;
+    handlers[0x6A] = ExecuteLoad_L_D;
+    handlers[0x6B] = ExecuteLoad_L_E;
+    handlers[0x6C] = ExecuteLoad_L_H;
+    handlers[0x6D] = ExecuteLoad_L_L;
+    handlers[0x6F] = ExecuteLoad_L_A;
+
     handlers[0x70] = ExecuteLoadIndirectHLFromB;
     handlers[0x71] = ExecuteLoadIndirectHLFromC;
     handlers[0x72] = ExecuteLoadIndirectHLFromD;
@@ -62,6 +109,13 @@ InstructionHandler ExecuteNoop(AddressBus& /*addressBus*/, CpuRegisters& /*cpuRe
     handlers[0x74] = ExecuteLoadIndirectHLFromH;
     handlers[0x75] = ExecuteLoadIndirectHLFromL;
     handlers[0x77] = ExecuteLoadIndirectHLFromA;
+    handlers[0x78] = ExecuteLoad_A_B;
+    handlers[0x79] = ExecuteLoad_A_C;
+    handlers[0x7A] = ExecuteLoad_A_D;
+    handlers[0x7B] = ExecuteLoad_A_E;
+    handlers[0x7C] = ExecuteLoad_A_H;
+    handlers[0x7D] = ExecuteLoad_A_L;
+    handlers[0x7F] = ExecuteLoad_A_A;
 
     return handlers;
 }
