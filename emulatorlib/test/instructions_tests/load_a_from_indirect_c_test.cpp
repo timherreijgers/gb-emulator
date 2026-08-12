9e65f99ef36c05868ca86578bd5ba724a7e2af76
Tim Herreijgers <timtim3001@gmail.com>
2026-08-12 09:12:10 +0200

    Added load indirect from and to C opcodes

---
+++ emulatorlib/test/instructions_tests/load_a_from_indirect_c_test.cpp
@@ -0,0 +1,49 @@
+/*
+ * Copyright © 2026. Tim Herreijgers
+ * Licensed using the MIT license
+ */
+
+#include "emulatorlib/address_bus.h"
+#include "emulatorlib/byte_utils.h"
+#include "emulatorlib/cpu.h"
+#include "emulatorlib/instruction_translation.h"
+#include "emulatorlib/test/address_bus_addressable_mock.h"
+
+#include "instruction_handlers/register_io_helpers.h"
+
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+
+namespace EmulatorLib::Test
+{
+
+class LoadAFromIndirectCTest : public ::testing::Test
+{
+protected:
+    ::testing::NiceMock<AddressBusAddressableMock> m_addressableMock;
+    AddressBus m_bus{{m_addressableMock}};
+    Cpu m_cpu{m_bus};
+};
+
+TEST_F(LoadAFromIndirectCTest, ExecutingOpCode)
+{
+    ON_CALL(m_addressableMock, ReadFromAddress(0x0100)).WillByDefault(::testing::Return(0xF2_b));
+    ON_CALL(m_addressableMock, ReadFromAddress(0x0101)).WillByDefault(::testing::Return(0x00_b));
+
+    const auto initialValue = m_cpu.Registers().accumulator.value;
+    m_cpu.Registers().cRegister = 0x77_b;
+
+    m_cpu.Step();
+    ASSERT_THAT(m_cpu.Registers().instructionRegister, ::testing::Eq(0xF2_b));
+    ASSERT_THAT(m_cpu.Registers().accumulator, ::testing::Eq(initialValue));
+
+    EXPECT_CALL(m_addressableMock, ReadFromAddress(0xFF77)).Times(1).WillOnce(::testing::Return(0x4A_b));
+    m_cpu.Step();
+    ASSERT_THAT(m_cpu.Registers().accumulator, ::testing::Eq(0x4A_b));
+
+    EXPECT_CALL(m_addressableMock, ReadFromAddress(0x101)).Times(1).WillOnce(::testing::Return(0x00_b));
+    m_cpu.Step();
+    ASSERT_THAT(m_cpu.Registers().instructionRegister, ::testing::Eq(0x00_b));
+}
+
+} // namespace EmulatorLib::Test
---
+++ emulatorlib/test/instructions_tests/load_indirect_c_from_a_test.cpp
@@ -0,0 +1,46 @@
+/*
+ * Copyright © 2026. Tim Herreijgers
+ * Licensed using the MIT license
+ */
+
+#include "emulatorlib/address_bus.h"
+#include "emulatorlib/byte_utils.h"
+#include "emulatorlib/cpu.h"
+#include "emulatorlib/instruction_translation.h"
+#include "emulatorlib/test/address_bus_addressable_mock.h"
+
+#include "instruction_handlers/register_io_helpers.h"
+
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+
+namespace EmulatorLib::Test
+{
+
+class LoadIndirectCTestFromA : public ::testing::Test
+{
+protected:
+    ::testing::NiceMock<AddressBusAddressableMock> m_addressableMock;
+    AddressBus m_bus{{m_addressableMock}};
+    Cpu m_cpu{m_bus};
+};
+
+TEST_F(LoadIndirectCTestFromA, ExecutingOpCode)
+{
+    ON_CALL(m_addressableMock, ReadFromAddress(0x0100)).WillByDefault(::testing::Return(0xE2_b));
+    ON_CALL(m_addressableMock, ReadFromAddress(0x0101)).WillByDefault(::testing::Return(0x00_b));
+
+    m_cpu.Registers().accumulator = 0x88_b;
+    m_cpu.Registers().cRegister = 0x66_b;
+
+    m_cpu.Step();
+    ASSERT_THAT(m_cpu.Registers().instructionRegister, ::testing::Eq(0xE2_b));
+
+    EXPECT_CALL(m_addressableMock, WriteToAddress(0xFF66, 0x88_b)).Times(1);
+    m_cpu.Step();
+
+    m_cpu.Step();
+    ASSERT_THAT(m_cpu.Registers().instructionRegister, ::testing::Eq(0x00_b));
+}
+
+} // namespace EmulatorLib::Test
