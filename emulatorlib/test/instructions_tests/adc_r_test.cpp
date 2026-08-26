17a09d765c8857c18c9e75b639d09753a8501204
Tim Herreijgers <timtim3001@gmail.com>
2026-08-26 23:17:47 +0200

    Added Adc_r and adc_n8 instructions

---
+++ emulatorlib/test/instructions_tests/adc_r_test.cpp
@@ -0,0 +1,184 @@
+/*
+ * Copyright © 2026. Tim Herreijgers
+ * Licensed using the MIT license
+ */
+
+#include "emulatorlib/cpu_flags.h"
+#include "emulatorlib/instruction_translation.h"
+#include "instruction_handlers/register_io_helpers.h"
+#include "instruction_test_base.h"
+
+namespace EmulatorLib::Test
+{
+
+namespace
+{
+
+template <typename T>
+[[nodiscard]] auto FunctionWrapper8Bit() -> std::function<Register8Bit&(CpuRegisters&)>
+{
+    return T{};
+}
+
+struct InstructionPair
+{
+    std::byte instruction;
+    std::function<Register8Bit&(CpuRegisters&)> sourceRegister;
+};
+
+} // namespace
+
+class AdcRTest : public InstructionTestBase, public ::testing::WithParamInterface<InstructionPair>
+{
+protected:
+    void SetUp() override
+    {
+        EXPECT_CALL(m_addressableMock, ReadFromAddress(::testing::Le(0x7FFF)))
+            .WillRepeatedly(::testing::Return(0x00_b));
+    }
+};
+
+TEST_P(AdcRTest, ExecutingOpCode)
+{
+    const auto& [instruction, sourceRegister] = GetParam();
+    m_program.WriteProgram({instruction, 0x00_b});
+
+    m_cpu.Registers().accumulator = 0x02_b;
+    sourceRegister(m_cpu.Registers()) = 0x03_b;
+    m_cpu.Registers().flags = 0x00_b;
+
+    m_cpu.Step();
+    ASSERT_THAT(m_cpu.Registers().instructionRegister, ::testing::Eq(instruction));
+    ASSERT_THAT(m_cpu.Registers().accumulator, ::testing::Eq(0x02_b));
+    ASSERT_THAT(sourceRegister(m_cpu.Registers()), ::testing::Eq(0x03_b));
+
+    m_cpu.Step();
+    ASSERT_THAT(m_cpu.Registers().accumulator, ::testing::Eq(0x05_b));
+    ASSERT_THAT(m_cpu.Registers().instructionRegister, ::testing::Eq(0x00_b));
+}
+
+TEST_P(AdcRTest, ExecutingOpCode_WithCarryFlag)
+{
+    const auto& [instruction, sourceRegister] = GetParam();
+    m_program.WriteProgram({instruction, 0x00_b});
+
+    m_cpu.Registers().accumulator = 0x02_b;
+    sourceRegister(m_cpu.Registers()) = 0x03_b;
+    m_cpu.Registers().flags = CpuFlags::Carry.AsByte();
+
+    m_cpu.Step();
+    m_cpu.Step();
+    ASSERT_THAT(m_cpu.Registers().accumulator, ::testing::Eq(0x06_b));
+}
+
+TEST_P(AdcRTest, ExecutingOpCode_ClearsSubtractionFlag)
+{
+    const auto& [instruction, sourceRegister] = GetParam();
+    m_program.WriteProgram({instruction, 0x00_b});
+
+    m_cpu.Registers().flags = CpuFlags::Subtract.AsByte();
+    m_cpu.Registers().accumulator = 0x01_b;
+    sourceRegister(m_cpu.Registers()) = 0x02_b;
+
+    m_cpu.Step();
+    m_cpu.Step();
+    ASSERT_THAT(m_cpu.Registers().flags, ::testing::Eq(0x00_b));
+}
+
+TEST_P(AdcRTest, ExecutingOpCode_SetsZeroFlagTo1)
+{
+    const auto& [instruction, sourceRegister] = GetParam();
+    m_program.WriteProgram({instruction, 0x00_b});
+
+    m_cpu.Registers().flags = 0x00_b;
+    m_cpu.Registers().accumulator = 0x00_b;
+    sourceRegister(m_cpu.Registers()) = 0x00_b;
+
+    m_cpu.Step();
+    m_cpu.Step();
+    ASSERT_THAT(m_cpu.Registers().flags, ::testing::Eq(CpuFlags::Zero.AsByte()));
+}
+
+TEST_P(AdcRTest, ExecutingOpCode_SetsZeroFlagTo0)
+{
+    const auto& [instruction, sourceRegister] = GetParam();
+    m_program.WriteProgram({instruction, 0x00_b});
+
+    m_cpu.Registers().flags = CpuFlags::Zero.AsByte();
+    m_cpu.Registers().accumulator = 0x01_b;
+    sourceRegister(m_cpu.Registers()) = 0x02_b;
+
+    m_cpu.Step();
+    m_cpu.Step();
+    ASSERT_THAT(m_cpu.Registers().flags, ::testing::Eq(0x00_b));
+}
+
+TEST_P(AdcRTest, ExecutingOpCode_SetsHalfCarryTo0)
+{
+    const auto& [instruction, sourceRegister] = GetParam();
+    m_program.WriteProgram({instruction, 0x00_b});
+
+    m_cpu.Registers().flags = CpuFlags::HalfCarry.AsByte();
+    m_cpu.Registers().accumulator = 0x01_b;
+    sourceRegister(m_cpu.Registers()) = 0x02_b;
+
+    m_cpu.Step();
+    m_cpu.Step();
+    ASSERT_THAT(m_cpu.Registers().flags, ::testing::Eq(0x00_b));
+}
+
+TEST_P(AdcRTest, ExecutingOpCode_SetsHalfCarryTo1)
+{
+    const auto& [instruction, sourceRegister] = GetParam();
+    m_program.WriteProgram({instruction, 0x00_b});
+
+    m_cpu.Registers().flags = 0x00_b;
+    m_cpu.Registers().accumulator = 0x0F_b;
+    sourceRegister(m_cpu.Registers()) = 0x01_b;
+
+    m_cpu.Step();
+    m_cpu.Step();
+    ASSERT_THAT(m_cpu.Registers().flags, ::testing::Eq(CpuFlags::HalfCarry.AsByte()));
+}
+
+TEST_P(AdcRTest, ExecutingOpCode_SetsCarryTo0)
+{
+    const auto& [instruction, sourceRegister] = GetParam();
+    m_program.WriteProgram({instruction, 0x00_b});
+
+    m_cpu.Registers().flags = 0x00_b;
+    m_cpu.Registers().accumulator = 0x10_b;
+    sourceRegister(m_cpu.Registers()) = 0x10_b;
+
+    m_cpu.Step();
+    m_cpu.Step();
+    ASSERT_THAT(m_cpu.Registers().flags, ::testing::Eq(0x00_b));
+}
+
+TEST_P(AdcRTest, ExecutingOpCode_SetsCarryTo1)
+{
+    const auto& [instruction, sourceRegister] = GetParam();
+    m_program.WriteProgram({instruction, 0x00_b});
+
+    m_cpu.Registers().flags = 0x00_b;
+    m_cpu.Registers().accumulator = 0xF0_b;
+    sourceRegister(m_cpu.Registers()) = 0x20_b;
+
+    m_cpu.Step();
+    m_cpu.Step();
+    ASSERT_THAT(m_cpu.Registers().flags, ::testing::Eq(CpuFlags::Carry.AsByte()));
+}
+
+INSTANTIATE_TEST_SUITE_P(AdcRTest, AdcRTest,
+                         ::testing::Values(
+                             InstructionPair{0x88_b, FunctionWrapper8Bit<RegisterB>()},
+                             InstructionPair{0x89_b, FunctionWrapper8Bit<RegisterC>()},
+                             InstructionPair{0x8A_b, FunctionWrapper8Bit<RegisterD>()},
+                             InstructionPair{0x8B_b, FunctionWrapper8Bit<RegisterE>()},
+                             InstructionPair{0x8C_b, FunctionWrapper8Bit<RegisterH>()},
+                             InstructionPair{0x8D_b, FunctionWrapper8Bit<RegisterL>()}),
+                         [](const testing::TestParamInfo<AdcRTest::ParamType>& info) {
+                             return std::string{OpCodeToInstructionName(info.param.instruction)};
+                         });
+
+} // namespace EmulatorLib::Test
