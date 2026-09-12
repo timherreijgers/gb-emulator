e209a9be32fbbb3d596a851f7437d7dc1b45272a
Tim Herreijgers <timtim3001@gmail.com>
2026-09-12 10:48:24 +0200

    Created generic math_r template

--- utilitylib/include/utilitylib/sub_with_borrow.h
+++ utilitylib/include/utilitylib/sub_with_borrow.h
@@ -10,6 +10,7 @@
 #include <concepts>
 #include <cstddef>
 #include <type_traits>
+#include <typeinfo>
 
 namespace UtilityLib
 {
@@ -18,23 +19,23 @@ template <typename T>
 concept SubWithBorrowOperand = std::integral<T> || std::is_same_v<T, std::byte>;
 
 template <SubWithBorrowOperand T>
-struct SubWithBorrowResult
+struct [[nodiscard]] SubWithBorrowResult
 {
     T result;
     T borrowBits;
 };
 
 // Implemented using Borrow-Lookahead subtractor algorithm
-template <SubWithBorrowOperand T>
-[[nodiscard]] constexpr auto SubWithBorrow(T left, T right) noexcept -> SubWithBorrowResult<T>
-{
-    const T result = static_cast<T>(left - right);
+constexpr auto SubWithBorrow = [](SubWithBorrowOperand auto left, SubWithBorrowOperand auto right) constexpr noexcept -> SubWithBorrowResult<decltype(left)> {
+    static_assert(typeid(decltype(left)) == typeid(decltype(right)));
+
+    const auto result = static_cast<decltype(left)>(left - right);
 
     const auto gamma = ~left & right;
     const auto pi = ~left | right;
 
-    const T borrowBits = static_cast<T>(gamma | (pi & result));
+    const auto borrowBits = static_cast<decltype(left)>(gamma | (pi & result));
     return {result, borrowBits};
-}
+};
 
 } // namespace UtilityLib
