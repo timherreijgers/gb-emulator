aeb7469ec36bbca2c32ecca75ef9da17fa8b9205
Tim Herreijgers <timtim3001@gmail.com>
2026-09-22 08:10:31 +0200

    Updated to c++ 26

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,6 +2,6 @@
 
 set -e
 
-conan install -pr:a=clang-22-linux-x86_64 -s compiler.cppstd=23 -s build_type=Release -c tools.cmake.cmaketoolchain:generator="Ninja" . --build=missing
-conan install -pr:a=clang-22-linux-x86_64 -s compiler.cppstd=23 -s build_type=RelWithDebInfo -c tools.cmake.cmaketoolchain:generator="Ninja" . --build=missing
-conan install -pr:a=clang-22-linux-x86_64 -s compiler.cppstd=23 -s build_type=Debug -c tools.cmake.cmaketoolchain:generator="Ninja" . --build=missing
+conan install -pr:a=clang-22-linux-x86_64 -s compiler.cppstd=26 -s build_type=Release -c tools.cmake.cmaketoolchain:generator="Ninja" . --build=missing
+conan install -pr:a=clang-22-linux-x86_64 -s compiler.cppstd=26 -s build_type=RelWithDebInfo -c tools.cmake.cmaketoolchain:generator="Ninja" . --build=missing
+conan install -pr:a=clang-22-linux-x86_64 -s compiler.cppstd=26 -s build_type=Debug -c tools.cmake.cmaketoolchain:generator="Ninja" . --build=missing
--- bootstrap_macos.sh
+++ bootstrap_macos.sh
@@ -2,6 +2,6 @@
 
 set -e
 
-conan install -pr:a=clang-22-macos-armv8 -s compiler.cppstd=23 -s build_type=Release -c tools.cmake.cmaketoolchain:generator="Ninja" . --build=missing
-conan install -pr:a=clang-22-macos-armv8 -s compiler.cppstd=23 -s build_type=RelWithDebInfo -c tools.cmake.cmaketoolchain:generator="Ninja" . --build=missing
-conan install -pr:a=clang-22-macos-armv8 -s compiler.cppstd=23 -s build_type=Debug -c tools.cmake.cmaketoolchain:generator="Ninja" . --build=missing
+conan install -pr:a=clang-22-macos-armv8 -s compiler.cppstd=26 -s build_type=Release -c tools.cmake.cmaketoolchain:generator="Ninja" . --build=missing
+conan install -pr:a=clang-22-macos-armv8 -s compiler.cppstd=26 -s build_type=RelWithDebInfo -c tools.cmake.cmaketoolchain:generator="Ninja" . --build=missing
+conan install -pr:a=clang-22-macos-armv8 -s compiler.cppstd=26 -s build_type=Debug -c tools.cmake.cmaketoolchain:generator="Ninja" . --build=missing
